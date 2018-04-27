c0d636a8cecdcbf04de7be26e24ba2ca9156abb8
Lukas Johannes Breitwieser <lukas.johannes.breitwieser@cern.ch>
2018-04-27 13:13:24 +0200

    Create scaffold for retina rendering

--- src/retina-rendering.h
+++ src/retina-rendering.h
@@ -5,26 +5,45 @@
 
 namespace bdm {
 
+BDM_SIM_OBJECT(RetinaCell, bdm::Cell) {
+  BDM_SIM_OBJECT_HEADER(RetinaCellExt, 1, color_);
+
+ public:
+  RetinaCellExt() : Base() {}
+  RetinaCellExt(const std::array<double, 3>& position, double diameter,
+                int color) {
+    Base::position_[kIdx] = position;
+    Base::diameter_[kIdx] = diameter;
+    color_[kIdx] = color;
+  }
+
+ private:
+  vec<int> color_;
+};
+
 // Define compile time parameter
 template <typename Backend>
-struct CompileTimeParam : public DefaultCompileTimeParam<Backend> {};
+struct CompileTimeParam : public DefaultCompileTimeParam<Backend> {
+  using AtomicTypes = VariadicTypedef<RetinaCell>;
+};
 
 inline int Simulate(int argc, const char** argv) {
   InitializeBioDynamo(argc, argv);
 
-  // Define initial model - in this example: single cell at origin
-  Cell cell({0, 0, 0});
-  cell.SetDiameter(30);
+  Param::export_visualization_ = true;
+  Param::visualize_sim_objects_["RetinaCell"] = {"color_"};
+
+  // TODO read csv file and create retina cells
+  RetinaCell cell({0, 0, 0}, 10, 0);
   ResourceManager<>::Get()->push_back(cell);
 
   // Run simulation for one timestep
   Scheduler<> scheduler;
   scheduler.Simulate(1);
-  
-  std::cout << "Simulation completed successfully!" << std::endl;
+
   return 0;
 }
 
-} // namespace bdm
+}  // namespace bdm
 
-#endif // RETINA_RENDERING_H_
+#endif  // RETINA_RENDERING_H_
